7c0d03ee83f5d44d4b8e156c8445569b94aff6f5
Barış Sokat <barissokat@outlook.com>
2021-12-30 14:03:52 +0300

    Update main page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import './App.css'
+import React, { useState } from 'react'
+
+import Home from './components/Home/Home'
+import Login from './components/Login/Login'
+import MainHeader from './components/MainHeader/MainHeader'
 
 function App () {
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+  const loginHandler = (email, password) => {
+    setIsLoggedIn(true)
+  }
+
+  const logoutHandler = () => {
+    setIsLoggedIn(false)
+  }
+
   return (
-    <div className='App'>
-      
-    </div>
+    <>
+      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+      <main>
+        {!isLoggedIn && <Login onLogin={loginHandler} />}
+        {isLoggedIn && <Home onLogout={logoutHandler} />}
+      </main>
+    </>
   )
 }
 
